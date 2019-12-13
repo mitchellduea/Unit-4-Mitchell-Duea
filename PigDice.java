d1db1b70aa0cd96ae016d397837cc6edf44ec209
mitchellduea <54951211+mitchellduea@users.noreply.github.com>
2019-12-13 09:16:16 -0800

    Add files via upload
    Updated Code for PigDice...

--- PigDice.java
+++ PigDice.java
@@ -10,7 +10,9 @@ public static Random generator = new Random ();
 
 public static int player1Score = 0;
 public static int player2Score = 0;
-
+// all players scores start at zero
+//Look for a place to add some of this stuff to the player class.
+//I need three classes to finish so I must addd something
 public static int counter;
 
 public static void main(String [] args){
@@ -20,7 +22,7 @@ public static void main(String [] args){
 
     player1Attributes ();
 }
-
+//I could maybe add some of this to the player class (Anything about the player )
 public static int Roll()
 {
     int Low = 1;
@@ -28,5 +30,22 @@ public static int Roll()
     int Roll = generator.nextInt (High - Low) + Low;
 
     return Roll;
-
+//I think I could add this to the dice class instead of having it in here
 }
+public static void p1Mechanics() {
+    counter = 0;
+    out.println("Would you like to play this turn...Yes!...No");
+    String answer = scan.nextLine ();
+    if (answer.equals("No")) {
+        system.out.println ("Looks like player 1 has skipped their turn:(");
+        system.out.println ("It is now player 2's turn to go");
+        player2Mechanics ();
+        //I think I need to keep this in here, cant move it to something else
+    }
+    //These are useing the roll mechanics so I need to keep it in here.
+    int Roll = Roll();
+    system.out.println ("Oh boy looks like you have rolled a" + Roll);
+    while (player1Score + counter < 100) {
+
+        while (Roll != 1) {
+          //Work on the while statement
