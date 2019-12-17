1cbc23dfaad074466f543cba56a7dab91080265e
mitchellduea <54951211+mitchellduea@users.noreply.github.com>
2019-12-17 09:46:11 -0800

    Add files via upload

---
+++ ZombieDie.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.Random;
+
+{
+Public ZombieDie
+	public static void main(String[] args)
+	{
+		int dice = 0
+		int rollDice =  0
+
+		int tempBrains	  = 0;
+		int tempShots	  = 0;
+		int currentPlayer = 0;
+		int playerCount	  = 0;
+		int userAction	  = 0;
+
+		clearScreen();
+		System.out.println("\n> Welcome to Zombie Dice ");
+		System.out.print("> Enter number of players: ");
