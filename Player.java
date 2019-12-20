12df69544c4c25a28964df2ce396531ed7781c72
mitchellduea <54951211+mitchellduea@users.noreply.github.com>
2019-12-19 20:18:43 -0800

    Add files via upload

---
+++ Player.java
@@ -0,0 +1,37 @@
+
+import java.util.random;
+
+
+import java.util.Scanner;
+
+
+
+
+import static java.lang.System.*;
+
+
+
+import java.util.*;
+
+
+public class Player{
+
+  public static void main(String[] args){
+
+
+
+    private int= score;
+
+    private int= getscore;
+
+    private int= addscore;
+
+    private int= dice;
+
+    private int= (Dice=2);
+
+    private int= counter;
+  }
+
+
+}
